6fb49206cd64901a38b5c44d3c5503fc83e7ab71
gmsebox <gms_ebox@yahoo.com>
2017-04-14 18:42:02 -0700

    update

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,11 +1,43 @@
 import { Component } from '@angular/core';
+import { ChartModule } from 'primeng/primeng';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title='My test';
   name ='';
+
+    data: any;
+    chart: any;
+options = {
+  responsive: false,
+  maintainAspectRatio: false
+};
+    constructor() {
+        this.data = {
+            labels: [],
+            datasets: [
+                {
+                    data: [300, 50, 100],
+                    backgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    hoverBackgroundColor: [
+                        "#FF6384",
+                        "#36A2EB",
+                        "#FFCE56"
+                    ],
+                    borderColor:[
+                        "#000000",
+                        "#000000",
+                        "#000000"
+                    ]
+                }]    
+            };
+    }
 }
--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -4,14 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import {ChartModule} from 'primeng/primeng';
 
 @NgModule({
-  declarations: [
-    AppComponent
+  declarations: [ 
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     FormsModule,
+    ChartModule,
     HttpModule
   ],
   providers: [],
